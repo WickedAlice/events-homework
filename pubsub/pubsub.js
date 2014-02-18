f5ac20046f7aec1f1899d7af33b78f8caac48c60
alisa <alisa.smyrnova@gmail.com>
2014-02-18 22:20:55 +0200

    new

--- pubsub/pubsub.js
+++ pubsub/pubsub.js
@@ -31,11 +31,17 @@ PubSub.prototype.subscribe = function(eventName, handler) {
  */
 PubSub.prototype.unsubscribe = function(eventName, handler) {
 
-    if (!this.handlersList[eventName]) {
+    var list = this.handlersList[eventName];
+
+    if (!handler) {
+        return undefined;
+    }
+
+    if (!list) {
         return handler;
     }
 
-    this.handlersList[eventName].splice( this.handlersList[eventName].indexOf(handler), 1);
+    list.splice(list.indexOf(handler), 1);
 
     return handler;
 };
@@ -49,14 +55,20 @@ PubSub.prototype.unsubscribe = function(eventName, handler) {
 PubSub.prototype.publish = function(eventName, data) {
 
     var i;
+    var list = this.handlersList[eventName];
 
-    if (this.handlersList[eventName]){
-        for ( i = 0; i < this.handlersList[eventName].length; i++ ) {
-            this.handlersList[eventName][i](data);
-        }
-        return true;
+    if (list){
+
+		for ( i = 0; i < list.length; i++ ) {
+  			(function(func){
+    			setTimeout(function(){
+      			func(eventName, data);
+    			}, 0);
+  			})(list[i]);
+		}
     }
     return false;
+    
 };
 
 /**
@@ -75,15 +87,40 @@ PubSub.prototype.off = function(eventName) {
 
 var pubSub = new PubSub();
 
-function a() {alert("aaa!");}
-function c() {alert("ccc!");}
-function b() {alert("bbb!");}
+function a() {console.log("111!");}
+function b() {console.log("222!");}
+function c() {
+    var d = 9;
+    for (var i = 0; i < 1000000000; i++ )
+    {d++;}
+    console.log ("done");
+}
+function d() {console.log("333!");}
+function f() {console.log("444!");}
+function g() {console.log("555!");}
+function h() {console.log("666!");}
+function j() {console.log("777!");}
+function k() {console.log("888!");}
+function l() {console.log("999!");}
+function q() {console.log("qqq!");}
+function w() {console.log("qqq!");}
 
 pubSub.subscribe('a', a);
 pubSub.subscribe('a', b);
 pubSub.subscribe('a', c);
+pubSub.subscribe('a', d);
+pubSub.subscribe('a', f);
+pubSub.subscribe('a', g);
+pubSub.subscribe('a', h);
+pubSub.subscribe('a', j);
+pubSub.subscribe('a', k);
+pubSub.subscribe('a', l);
+pubSub.subscribe('a', q);
+pubSub.subscribe('a', w);
 
 pubSub.publish('a');
+//console.log("faza 2");
+
 pubSub.unsubscribe('a', a);
 pubSub.publish('a');
 pubSub.off('a');
@@ -101,3 +138,4 @@ pubSub.publish('a');
  * //Отписать группу функций от события 'click'
  * PubSub.off('click');
  */
+
